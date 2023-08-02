a7904885f2a3b6842907a88da3109f0fa19f6124
azadeh.samadi <samadi.azadeh@gmail.com>
2023-08-02 09:40:30 +0200

    Initial commit

---
+++ src/main/java/com/workshopPart1/App.java
@@ -0,0 +1,26 @@
+package com.workshopPart1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        String[] myArray = new String[3];
+
+        myArray[0] = "Erik";
+        myArray[1] = " ";
+        myArray[2] = "Svensson";
+
+        for (String word : myArray) {
+            System.out.print(word);
+        }
+    }
+}
+
+
+
