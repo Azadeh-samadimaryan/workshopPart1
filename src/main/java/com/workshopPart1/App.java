4780701920d90b800f3b4f40fedd579bab51aa16
azadeh.samadi <samadi.azadeh@gmail.com>
2023-08-07 00:41:30 +0200

    workshopPart2

--- src/main/java/com/workshopPart1/App.java
+++ src/main/java/com/workshopPart1/App.java
@@ -6,21 +6,29 @@ package com.workshopPart1;
 public class App {
 
     static String[] names = new String[10];
-    
+
     public static void main(String[] args) {
 
-        fillNames("Erik Svensson",names);
-        fillNames("Mehrdad Javan",names);
-        fillNames("Omid Mojabi",names);
-        fillNames("Azadeh Samadi",names);
+        /*fillNames("Erik Svensson", names);
+        fillNames("Mehrdad Javan", names);
+        fillNames("Omid Mojabi", names);
+        fillNames("Azadeh Samadi", names);*/
 
-        printNames(names);
-        
+        add("Erik Svensson");
+        add("Mehrdad Javan");
+        //add("Erik Svensson");
+        //add("Omid Mojabi");
+        //add("Azadeh Samadi");
+
+        System.out.println(find("Omid Mojabi"));
+        System.out.println(find("Erik Svensson"));
+
+        //printNames(names);
+
     }
 
-    private static void printNames(String[] repoArray)
-    {
-        for (int i=0; i< repoArray.length; i++){
+    private static void printNames(String[] repoArray) {
+        for (int i = 0; i < repoArray.length; i++) {
 
             if (repoArray[i] != null && repoArray[i] != "") {
                 System.out.println(repoArray[i]);
@@ -29,15 +37,14 @@ public class App {
         }
     }
 
-    private static void fillNames(String name,String[] repository) {
+    private static void fillNames(String name, String[] repository) {
 
-        if (repository == null){
+        if (repository == null) {
             repository = new String[10];
-        }
-        else{
-            for (int i=0; i< repository.length; i++){
+        } else {
+            for (int i = 0; i < repository.length; i++) {
 
-                if (repository[i] == null || repository[i] ==""){
+                if (repository[i] == null || repository[i] == "") {
                     repository[i] = name;
                     break;
                 }
@@ -46,7 +53,35 @@ public class App {
 
     }
 
+    public static String find(final String fullName){
+        for (int i = 0; i < names.length; i++) {
+            if(names[i] == fullName){
+                return names[i] +" found successfully";
+            }
+        }
+        return fullName +" not found";
+    }
 
+    private static boolean add(final String fullName) {
+
+        for (int i = 0; i < names.length; i++) {
+            if(names[i] == fullName){
+                System.out.println(fullName + " is duplicated");
+                return false;
+            }
+        }
+
+        for (int i = 0; i < names.length; i++) {
+
+            if (names[i] == null || names[i] == "") {
+                names[i] = fullName;
+                System.out.println(fullName + " added successfully");
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
 
 
